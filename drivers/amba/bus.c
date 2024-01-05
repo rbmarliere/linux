10175906f1d2e8e63ffff58dc2d840aff24e5d67
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2024-01-05 13:00:47 +0100

    FIXME: amba: make amba_bustype constant
    Now that the driver core can properly handle constant struct bus_type,
    move the amba_bustype variable to be a constant structure as well,
    placing it into read-only memory which can not be modified at runtime.
    
    ************************FIXME *********************************************
    This really doesn't do the marking, drivers/iommu/iommu.c wants to write
    to the iommu_ops structure of a number of bus types, including the
    amba_bustype, so that needs to be fixed up first before this can be done
    properly.
    ************************FIXME *********************************************
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/amba/bus.c
+++ drivers/amba/bus.c
@@ -435,6 +435,7 @@ static const struct dev_pm_ops amba_pm = {
  * DMA configuration for platform and AMBA bus is same. So here we reuse
  * platform's DMA config routine.
  */
+//const struct bus_type amba_bustype = {
 struct bus_type amba_bustype = {
 	.name		= "amba",
 	.dev_groups	= amba_dev_groups,
